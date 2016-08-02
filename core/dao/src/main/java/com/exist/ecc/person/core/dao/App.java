d8d5fea899eaff471f51283f7b85f578b4d85068
James Stephen DV Mariano <james.stephen.dv14@gmail.com>
2016-08-02 13:59:58 +0800

    Try out simple Hibernate session code in dao module

---
+++ core/dao/src/main/java/com/exist/ecc/person/core/dao/App.java
@@ -0,0 +1,54 @@
+package com.exist.ecc.person.core.dao;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.exist.ecc.person.util.HibernateUtil;
+
+import com.exist.ecc.person.core.model.Person;
+import com.exist.ecc.person.core.model.Name;
+import com.exist.ecc.person.core.model.Address;
+
+public class App {
+
+  public static void main( String[] args ) {
+    Session session = HibernateUtil.getSessionFactory().openSession();
+
+    try {
+      Transaction transaction = session.beginTransaction();
+      
+      Person diene = new Person();
+      Name name = new Name();
+      Address address = new Address();
+
+      diene.setBirthDate(new Date());
+      diene.setGwa(new BigDecimal("1.77"));
+      diene.setDateHired(new Date());
+      diene.setEmployed(false);
+
+      name.setFirstName("Diene");
+      name.setTitle("Dr.");
+
+      address.setStreetAddress("33 N, SF");
+      address.setBarangay("DM");
+      address.setMunicipality("QC");
+
+      diene.setName(name);
+      diene.setAddress(address);
+
+      session.save(diene);
+      
+      transaction.commit();
+      
+    } catch (Exception e) {
+      e.printStackTrace();
+    } finally {
+      session.close();
+      HibernateUtil.getSessionFactory().close();
+    }
+  }
+
+}
---
+++ util/src/main/java/com/exist/ecc/person/util/HibernateUtil.java
@@ -0,0 +1,28 @@
+package com.exist.ecc.person.util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+
+  private static final SessionFactory sessionFactory = buildSessionFactory();
+  
+  private static SessionFactory buildSessionFactory() {
+    try {
+      Configuration configuration = new Configuration();
+      configuration.configure();
+      return configuration
+             .buildSessionFactory(new StandardServiceRegistryBuilder()
+             .applySettings(configuration.getProperties())
+             .build());
+    } catch (Exception e) {
+      e.printStackTrace();
+      throw new RuntimeException("there was an error building the factory");
+    }
+  }
+
+  public static SessionFactory getSessionFactory() {
+    return sessionFactory;
+  }
+}
