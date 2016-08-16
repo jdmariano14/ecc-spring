5ac900277c2e9d7498ae87e46e35589ccb14c429
James Stephen DV Mariano <james.stephen.dv14@gmail.com>
2016-08-16 09:19:02 +0800

    Delete unused models

--- core/model/src/main/java/com/exist/ecc/person/core/model/EmailAddress.java
+++
@@ -1,9 +0,0 @@
-package com.exist.ecc.person.core.model;
-
-public class EmailAddress extends ContactDetail {
-
-  protected boolean isValidDetail(String detail) {
-    throw new UnsupportedOperationException();
-  }
-
-}
