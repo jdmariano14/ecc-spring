26f2b8adb78b9488480694f00a4b6eb4f58b1222
James Stephen DV Mariano <james.stephen.dv14@gmail.com>
2016-08-16 11:21:12 +0800

    Add annotations for Role CRUD

--- core/model/src/main/java/com/exist/ecc/person/core/model/Role.java
+++ core/model/src/main/java/com/exist/ecc/person/core/model/Role.java
@@ -2,12 +2,26 @@ package com.exist.ecc.person.core.model;
 
 import java.util.Collection;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Column;
+import javax.persistence.Transient;
+
+@Entity
+@Table(name = "ROLE")
 public class Role {
 
+  @Id
+  @GeneratedValue
+  @Column(name = "ROLE_ID")
   private long roleId;
   
+  @Column(name = "NAME")
   private String name;
 
+  @Transient
   private Collection<Person> persons;
 
   public long getRoleId() {
