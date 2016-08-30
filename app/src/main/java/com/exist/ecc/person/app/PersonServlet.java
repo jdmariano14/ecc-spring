19c175b7f1949ba340b214c248d431e5f277d677
James Stephen DV Mariano <james.stephen.dv14@gmail.com>
2016-08-30 15:44:22 +0800

    Display a Person's info by redirecting to a JSP template

--- app/src/main/java/com/exist/ecc/person/app/PersonServlet.java
+++ app/src/main/java/com/exist/ecc/person/app/PersonServlet.java
@@ -5,6 +5,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.hibernate.Session;
 
@@ -13,6 +14,7 @@ import com.exist.ecc.person.core.dao.Transactions;
 import com.exist.ecc.person.core.dao.impl.PersonCriteriaDao;
 
 import com.exist.ecc.person.core.model.Person;
+import com.exist.ecc.person.core.model.Name;
  
 public class PersonServlet extends HttpServlet {
 
@@ -25,23 +27,10 @@ public class PersonServlet extends HttpServlet {
 
     try {
       long personId = getPersonId(req.getRequestURI());
+
       if (personId < 0) {
-
       } else {
-        Session session = Sessions.getSession();
-
-        try {
-          Person person = Transactions.get(session, personDao, () ->
-            personDao.get(personId));
-
-          res.setStatus(HttpServletResponse.SC_OK);
-          res.getWriter().println("<h1>" + person.getName().getFirstName() + "</h1>");
-          res.getWriter().println("<p><a href=\"/persons\">back</a></p>");
-        } catch (Exception e) {
-          throw new RuntimeException(e);
-        } finally {
-          session.close();
-        }
+        show(req, res, personId);
       }
 
     } catch (RuntimeException e) {
@@ -49,6 +38,28 @@ public class PersonServlet extends HttpServlet {
       res.getWriter().println("<h1>Error</h1>");
       res.getWriter().println("<p>" + e.getMessage() + "</p>");
       res.getWriter().println("<p><a href=\"/persons\">back</a></p>");
+    }
+  }
+
+  private void show(HttpServletRequest req, HttpServletResponse res, 
+    long personId) 
+  {
+    Session dbSession = Sessions.getSession();
+
+    try {
+      HttpSession httpSession = req.getSession();
+      
+      Person person = Transactions.get(dbSession, personDao, () ->
+        personDao.get(personId));
+      Name name = person.getName();
+
+      req.setAttribute("firstname", name.getFirstName());
+      req.getRequestDispatcher("/WEB-INF/views/persons/show.jsp")
+         .forward(req, res);
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    } finally {
+      dbSession.close();
     }
   }
 
