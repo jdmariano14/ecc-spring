d4ea103ef3971629e621705e15fb953f7847ba0d
James DV <azyriel14@gmail.com>
2016-09-15 20:11:29 +0800

    Remove unnecessary dao and service classes

--- core/dao/src/main/java/com/exist/ecc/person/core/dao/api/ContactDao.java
+++
@@ -1,9 +0,0 @@
-package com.exist.ecc.person.core.dao.api;
-
-import com.exist.ecc.person.core.model.Contact;
-
-public interface ContactDao extends Dao<Contact, Long> {
-  
-  // insert Contact-specific methods here
-  
-}
