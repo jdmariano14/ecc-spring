e992c06a934e6cc4e726a4f4eea3503a946dbe7d
James DV <azyriel14@gmail.com>
2016-09-19 06:34:51 +0800

    Connect using HikariCP

--- core/dao/src/main/java/com/exist/ecc/person/core/dao/HibernateSetup.java
+++ core/dao/src/main/java/com/exist/ecc/person/core/dao/HibernateSetup.java
@@ -4,19 +4,12 @@ import org.hibernate.service.ServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 
+import com.exist.ecc.person.infra.persistence.HibernateConfig;
+
 public class HibernateSetup {
 
   public static Configuration getConfiguration() {
-    Configuration configuration = 
-      new Configuration()
-      .setProperty("hibernate.connection.url", System.getenv("DB_URL"))
-      .setProperty("hibernate.connection.username", System.getenv("DB_USER"))
-      .setProperty("hibernate.connection.password", System.getenv("DB_PASS"))
-      .setProperty("hibernate.enable_lazy_load_no_trans", "true");
-
-    configuration.configure();
-
-    return configuration;
+    return HibernateConfig.get();
   }
 
   public static ServiceRegistry getServiceRegistry() {
